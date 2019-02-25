b33f573add76ddabf52e90c53cc0bdebaa0ef61f
huangzepeng <huangzepengs@163.com>
2019-02-25 20:28:35 +0800

    再次提交

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
         System.out.println("第一次远程仓库提交");
+        System.out.println("再次远程提交");
     }
 }
