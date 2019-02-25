611632fde158959cfae49c7bc7123c8dd1c724f0
huangzepeng <huangzepengs@163.com>
2019-02-25 21:42:57 +0800

    idea修改 本地提交

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -5,7 +5,5 @@ public class Main {
     @Test
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
-        System.out.println("第一次远程仓库提交");
-        System.out.println("再次远程提交");
     }
 }
